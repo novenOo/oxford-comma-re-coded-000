539d69b8d31ac34375f8dbd9585e4d4df03ecdae
noven temer <noven5823@gmail.com>
2016-12-20 17:56:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.size ==1
+    array[0]
+  elsif array.size ==2
+    array.join( " and ")
+  else
+    last=array.pop
+   arr= array.join(", ")
+  arr+= array[-1]= ", and " + last
+  end
 
 end
